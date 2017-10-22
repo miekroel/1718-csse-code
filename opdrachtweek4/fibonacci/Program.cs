c034cdd165abd973037713f6b309ee9f45f184e8
miekroel <mieke.roelandt@student.arteveldehs.be>
2017-10-22 19:49:15 +0200

    Add files via upload

---
+++ opdrachtweek4/fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace week4 {
+    class Program {
+        static void Main(string[] args) {
+            int result = geefWillekeurigGetal();
+            uitvoerenFibonacci(result);
+        }
+        
+        static int geefWillekeurigGetal() {
+            Console.WriteLine("lengte reeks fibonacci?");
+            int lengte = Convert.ToInt32((Console.ReadLine()));
+            return lengte;
+        }
+
+        static void uitvoerenFibonacci(int result) {
+            int[] a = new int[result];
+            a[0] = 0;
+            a[1] = 1;
+            for (int i = 2; i < result; i++) {
+                a[i] = a[i - 1] + a[i - 2];
+            }
+            int teller = 0;
+            foreach(int j in a) {
+                Console.WriteLine(a[teller]);
+                teller++;
+            } 
+        }
+    }
+}
---
+++ opdrachtweek4/priemgetal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace week4
+{
+    class Program
+    {
+
+        static void Main(string[] args){
+            int number = GeefGetal();
+            if (number < 100){
+                //Console.WriteLine(IsPriemgetal(GeefGetal()));
+                for(int i = 0; i < (number+1); i++){
+                    Console.WriteLine(i + IsPriemgetal(i));
+                }
+            }
+            else{
+                Console.WriteLine("het getal is niet kleiner dan 100");
+            }
+        }
+
+        static int GeefGetal ()
+        {
+            Console.WriteLine("geef een getal in kleiner dan 100");
+            int getal = Convert.ToInt32((Console.ReadLine()));
+            return getal;
+        }    
+
+        static string IsPriemgetal(int getal){
+            int tllr = 0;
+            for (int i = 1; i < (getal+1); i++){
+                if((getal%i) == 0){
+                    //Console.WriteLine("{0} is deelbaar door: {1}", getal, i);
+                    tllr++;
+                }
+            }
+            if(tllr == 2){
+                return " is een priemgetal";
+            }else{
+                return " is geen priemgetal";
+ 
+            }
+            
+        }
+    }
+}
